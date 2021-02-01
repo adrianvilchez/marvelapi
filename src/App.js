00fc33050fa2564dbc0a596f00e7e2650ec45a89
adrianvilchez <avilchez758@gmail.com>
2021-02-01 18:45:56 +0100

    Obtenemos los datos de la api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import react from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const consultarAPI = async () => {
+
+    const busqueda = 'characters';
+    const limit = '100';
+    const publicKey = 'f9661b672497c854c70e9c932a8c9d23';
+    const hash = '84f0b99511bd68b7258e68d7fdd05dec';
+
+    const api = await fetch(`http://gateway.marvel.com/v1/public/${busqueda}?limit=${limit}&ts=1000&apikey=${publicKey}&hash=${hash}`);
+    const datos = await api.json();
+
+    datos.data.results.map(item =>{
+        console.log(item.name);
+    });
+  }
+
   return (
-    <h1> Héroes de MARVEL </h1>
+    <div>
+      <h1> Héroes de MARVEL</h1>
+
+      <button
+        onClick = { consultarAPI }
+      > 
+          Consultar
+      </button>
+    </div>
   );
 }
 
